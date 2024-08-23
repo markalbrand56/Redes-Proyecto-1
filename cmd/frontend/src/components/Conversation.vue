e70fc1820588b5ff6e215aa6bb636c219f5e3f50
Mark Albrand <mark.albrand5@gmail.com>
2024-08-22 23:26:45 -0600

    Styles revisited

--- cmd/frontend/src/components/Conversation.vue
+++ cmd/frontend/src/components/Conversation.vue
@@ -24,25 +24,18 @@ const props = defineProps({
 
 <template>
   <div class="conversation-container">
-    <Message v-for="message in props.messages" :key="message.timestamp" :message="message"  :user="props.user" :is-conference="props.isConference"/>
+    <Message
+        v-for="message in props.messages"
+        :key="message.timestamp"
+        :message="message"
+        :user="props.user"
+        :is-conference="props.isConference"
+    />
   </div>
 </template>
 
 <style scoped>
-
 .conversation-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-
-  height: fit-content;
-  min-height: 100%;
-
-  gap: 1rem;
-  background-color: #383838;
-  padding-top: 5rem;
-  overflow-y: hidden;
-
+  @apply flex flex-col justify-end h-fit min-h-full gap-4 bg-[#383838] pt-20 overflow-y-hidden;
 }
-
 </style>
