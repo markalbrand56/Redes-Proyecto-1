843bab3e33c0990829be0bb058b4d10f26ccd8c5
Mark Albrand <mark.albrand5@gmail.com>
2024-08-23 16:07:46 -0600

    Documentation

--- cmd/app.go
+++ cmd/app.go
@@ -18,8 +18,7 @@ func NewApp() *App {
 	return &App{}
 }
 
-// startup is called when the app starts. The context is saved
-// so we can call the runtime methods
+// startup is called when the app starts
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 
@@ -27,18 +26,22 @@ func (a *App) startup(ctx context.Context) {
 	//a.Login("alb21004@alumchat.lol", "mark123")
 }
 
+// OnShutdown is called when the app is shutting down
 func (a *App) OnShutdown(ctx context.Context) {
 	chat.Close()
 }
 
+// Login logs in the user with the given username and password
 func (a *App) Login(username string, password string) {
 	chat.Start(a.ctx, username, password)
 }
 
+// Logout logs out the user
 func (a *App) Logout() {
 	chat.LogoutChannel <- true
 }
 
+// SendMessage sends a message to the given user
 func (a *App) SendMessage(body string, to string, from string) {
 	message := models.NewMessage(body, to, from)
 
@@ -47,6 +50,7 @@ func (a *App) SendMessage(body string, to string, from string) {
 	chat.TextChannel <- message
 }
 
+// SendFileMessage sends a file message to the given user
 func (a *App) SendFileMessage(file string, to string, from string) {
 	message := models.NewMessage(file, to, from)
 
@@ -55,6 +59,7 @@ func (a *App) SendFileMessage(file string, to string, from string) {
 	chat.FileChannel <- message
 }
 
+// SendConferenceMessage sends a message to the given conference
 func (a *App) SendConferenceMessage(body string, to string, from string) {
 	message := models.NewMessage(body, to, from)
 
@@ -63,6 +68,7 @@ func (a *App) SendConferenceMessage(body string, to string, from string) {
 	chat.ConferenceTextChannel <- message
 }
 
+// SendConferenceFileMessage sends a file message to the given conference
 func (a *App) SendConferenceFileMessage(file string, to string, from string) {
 	message := models.NewMessage(file, to, from)
 
@@ -71,10 +77,12 @@ func (a *App) SendConferenceFileMessage(file string, to string, from string) {
 	chat.ConferenceFileChannel <- message
 }
 
+// GetContacts returns the contacts of the user
 func (a *App) GetContacts() []string {
 	return chat.User.Contacts
 }
 
+// GetMessages returns the messages of the given user
 func (a *App) GetMessages(username string) []models.Message {
 	//chat.User.ShowConversations()
 
@@ -87,6 +95,7 @@ func (a *App) GetMessages(username string) []models.Message {
 	return r
 }
 
+// GetMessagesConference returns the messages of the given conference
 func (a *App) GetMessagesConference(jid string) []models.Message {
 	u := chat.User
 	if _, ok := u.Conferences[jid]; !ok {
@@ -98,31 +107,38 @@ func (a *App) GetMessagesConference(jid string) []models.Message {
 	return chat.User.Conferences[jid].Messages
 }
 
+// UpdateContacts updates the contacts of the user from the server
 func (a *App) UpdateContacts() {
 	chat.FetchContactsChannel <- true
 }
 
+// RequestContact sends a contact request to the given username
 func (a *App) RequestContact(username string) {
 	chat.SubscribeToChannel <- username
 }
 
+// AcceptSubscription accepts the subscription request of the given username
 func (a *App) AcceptSubscription(username string) {
 	// Se ha decidido aceptar la solicitud de suscripción
 	chat.SubscriptionRequestChannel <- username
 }
 
+// AcceptConferenceInvitation accepts the conference invitation of the given JID
 func (a *App) AcceptConferenceInvitation(jid string) {
 	chat.ConferenceInvitationChannel <- jid
 }
 
+// CancelSubscription cancels the subscription of the given username
 func (a *App) CancelSubscription(username string) {
 	chat.UnsubscribeFromChannel <- username
 }
 
+// RejectSubscription rejects the subscription of the given username
 func (a *App) RejectSubscription(username string) {
 	chat.RejectionChannel <- username
 }
 
+// SetStatus sets the status of the user
 func (a *App) SetStatus(status int) {
 	switch status {
 	case 0: // Online
@@ -140,14 +156,17 @@ func (a *App) SetStatus(status int) {
 	}
 }
 
+// GetArchive gets the archive of the given username
 func (a *App) GetArchive(username string) {
 	chat.FetchArchiveChannel <- username
 }
 
+// GetCurrentUser returns the current user
 func (a *App) GetCurrentUser() string {
 	return chat.User.UserName
 }
 
+// ProbeContacts probes the contacts of the user to fetch their status
 func (a *App) ProbeContacts() {
 	chat.ProbeContactsChannel <- true
 }
