57f7f14924ab8c92d13dce72d9713e6147585e02
Mark Albrand <mark.albrand5@gmail.com>
2024-08-23 01:17:16 -0600

    Loader on images

--- cmd/frontend/src/components/Message.vue
+++ cmd/frontend/src/components/Message.vue
@@ -1,7 +1,7 @@
 <script setup>
-
-import {reactive} from 'vue'
-import {models} from "../../wailsjs/go/models.ts";
+import { reactive, ref, onMounted } from 'vue'
+import { models } from "../../wailsjs/go/models.ts";
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 
 const message = reactive({
   body: "",
@@ -26,17 +26,37 @@ const props = defineProps({
 message.body = props.message.body
 message.timestamp = props.message.timestamp.slice(5, 16).replace("T", " ")
 
-const isUserMessage = (props.message.from.split("/")[1] === props.user.split("@")[0] )  || (props.message.from === props.user)
+const isUserMessage = (props.message.from.split("/")[1] === props.user.split("@")[0]) || (props.message.from === props.user)
 const sender = props.message.from.split("/")[1]
 
 const isImage = props.message.body.startsWith("https://") && (props.message.body.endsWith(".png") || props.message.body.endsWith(".jpg") || props.message.body.endsWith(".jpeg"))
+const imageLoaded = ref(false)
+
+function handleImageLoad() {
+  console.log("Image loaded")
+  imageLoaded.value = true
+}
+
+// Para asegurar que la imagen comience a cargar en `onMounted`
+onMounted(() => {
+  if (isImage) {
+    const img = new Image();
+    img.src = message.body;
+    img.onload = handleImageLoad;
+    img.onerror = () => {
+      console.error("Error loading image");
+      imageLoaded.value = true; // Evita el loader infinito en caso de error
+    }
+  }
+})
 
 </script>
 
 <template>
   <div :class="['message-container', isUserMessage ? 'user-message' : 'other-message']">
     <p v-if="isConference && !isUserMessage" class="message-sender"> {{ sender }} </p>
-    <img v-if="isImage" :src="message.body" alt="Image">
+    <PulseLoader v-if="isImage && !imageLoaded" color="#007bff" size="10px" class="mt-4 min-w-32"/>
+    <img v-if="isImage && imageLoaded" :src="message.body" alt="Image">
     <div class="inner-message">
       <p class="message-body" v-if="!isImage"> {{ message.body }}  </p>
       <p class="message-timestamp"> {{ message.timestamp }} </p>
@@ -77,4 +97,3 @@ const isImage = props.message.body.startsWith("https://") && (props.message.body
   @apply items-start justify-start;
 }
 </style>
-
