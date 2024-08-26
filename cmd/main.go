afc6c931ca76d191dad703a1de108ea4f37c9d73
Mark Albrand <mark.albrand5@gmail.com>
2024-08-25 19:54:41 -0600

    Logos

--- cmd/main.go
+++ cmd/main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:            "RedesProyecto",
+		Title:            "XMPP Chatting",
 		Width:            1024,
 		Height:           768,
 		WindowStartState: options.Maximised,
